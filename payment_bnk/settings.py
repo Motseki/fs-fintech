c32d8d0b2788adfcb7098542b1923d273f6104a6
Motseki <lbmotseki@gmail.com>
2024-02-27 07:30:46 +0200

    Latest Update

--- payment_bnk/settings.py
+++ payment_bnk/settings.py
@@ -17,7 +17,7 @@ import dj_database_url
 
 # env = environ.Env()
 env = environ.Env(DEBUG=(bool, False))
-
+#Installed Environ packeage
 
 # Set the project base directory
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
